d5adaa5aae79476ad05d95e0fde3ab75df6dc65c
Livia Pires <livia.pires@aluno.ifsp.edu.br>
2025-04-29 10:07:37 -0300

    Exemplo: contador + imagem

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
+import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [items, setItems] = useState([]);
+   const [text, setText] = useState('');
+   const [editItemId, setEditItemId] = useState(null);
+   const [editItemText, setEditItemText] = useState('');
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
+
+  const addItem = () => {
+    if (text.trim()==='') {
+      return;
+    }
+    const newItem = {
+      id: Math.radom().toString(),
+      text: text.trim(),
+    };
+    setItems([...items, newItem]);
+    setText('');
+    console.log(items);
+  };
+
+ 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        placeholder='Enter text item'
+      />
+      <Button title="Add Item" onPress={addItem} />
+      <Text style={styles.text}>Olá App React Native -  Atualiza!</Text>
       <StatusBar style="auto" />
+      <Image
+        source={{ uri: "https://picsum.photos//200" }}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text style={styles.text}>Counter: {counter}</Text>
+    
+    <View style={styles.buttonContainer}>
+      <Button title="Increment" onPress={incrementCounter} />
+      <Button title="Decrement" onPress={decrementCounter} />
+    </View>
     </View>
   );
 }
@@ -16,5 +63,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize: 24,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
   },
 });
