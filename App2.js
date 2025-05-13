aad9ffb01464d530bef1c70dbe67042b98097d2c
Livia Pires <livia.pires@aluno.ifsp.edu.br>
2025-05-13 09:14:35 -0300

    TextInput basic examples

--- App2.js
+++ App2.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
 import List from './components/List.js';
-
-import Inputs from './components/Inputs.js';
-
 export default function App() {
  
   return (
     <View style={styles.container}>
-     <Inputs />
+      <List />
+    <View style={styles.redbox}></View>
+    <View style={styles.bluebox}></View>
+    <View style={styles.blackbox}></View>
   </View>
   );
 }
@@ -19,7 +19,24 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: 'gray',
     height: 600,
     marginTop:150,
+  },
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    borderRadius: 50,
+  },
+  bluebox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+  },
+  blackbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
   },
 });
